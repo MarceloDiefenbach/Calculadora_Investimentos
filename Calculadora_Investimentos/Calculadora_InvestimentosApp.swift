7d0825c8fdafff68e85f70254bc969208896d898
Marcelo <lelod15@gmail.com>
2021-05-24 14:30:46 -0300

    Initial Commit

---
+++ Calculadora_Investimentos/Calculadora_InvestimentosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora_InvestimentosApp.swift
+//  Calculadora_Investimentos
+//
+//  Created by Marcelo Diefenbach on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculadora_InvestimentosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
